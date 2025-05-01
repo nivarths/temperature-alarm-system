229faaefebf7875c0862a44bac64771f8f7ab524
nivarths <nivarths@mcmaster.ca>
2025-05-01 00:13:15 -0400

    initial commit

---
+++ temperature_alarm_system.ino
@@ -0,0 +1,49 @@
+#include "DHT.h"
+
+#define DHT11_PIN 2
+#define DHTTYPE DHT11
+#define BUZZER 8
+#define TEMPERATURE_THRESHOLD 30.00
+
+// constructor for the sensor, args: pin number and type of sensor
+DHT dht11(DHT11_PIN, DHT11);
+
+void setup() 
+{
+  // initializing sensor and serial communication
+  Serial.begin(9600);
+  dht11.begin();
+  delay(2000);
+  pinMode(BUZZER, OUTPUT);
+}
+
+void loop() 
+{
+  float temperatureCelsius = dht11.readTemperature();
+  
+  if(isnan(temperatureCelsius))
+  {
+    Serial.println("Failed to detect temperature reading");
+    delay(2000);  
+    return;       
+  }
+  else
+  {
+    Serial.print("Temperature: ");
+    Serial.print(temperatureCelsius);
+    Serial.println(" °C");
+  }
+
+
+  if(temperatureCelsius >= TEMPERATURE_THRESHOLD)
+  {
+    tone(BUZZER, 440);
+  }
+  else
+  {
+    noTone(BUZZER);
+  }
+
+  delay(2000);
+
+}
